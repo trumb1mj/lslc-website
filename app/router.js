6f65c9d253b606e7412421add7923f213dcc5aac
Mike Trumbell <mtrumbell@optionscitysmbp.optionscity.com>
2015-12-04 11:15:16 -0600

    Redo website with ember

---
+++ app/router.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('home');
+  this.route('services');
+  this.route('new-clients');
+  this.route('contact');
+});
+
+export default Router;
