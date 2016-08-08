a3e72b623daa2b44782be9d80c66c3d24f93b54b
Mike Trumbell <mtrumbell@optionscitysmbp.optionscity.com>
2016-08-08 12:03:57 -0500

    Updated page description

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   rotateCarousel: function(){
     var self = this;
-    Ember.run.later((function() {
-      self.rotateCarousel()
-      self.get("carousel.swiper")._slideNext()
-    }), 8000);
+    // Ember.run.later((function() {
+    //   self.rotateCarousel()
+    //   self.get("carousel.swiper")._slideNext()
+    // }), 8000);
   },
   init: function(){
     this.rotateCarousel();
